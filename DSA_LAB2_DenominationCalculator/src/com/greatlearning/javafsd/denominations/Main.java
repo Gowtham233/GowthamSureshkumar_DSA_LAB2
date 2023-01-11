aa41e7e1471ee1f7c12a981d4d3f8ae67da6b006
Gowtham233 <65482558+Gowtham233@users.noreply.github.com>
2023-01-11 10:24:33 +0530

    Add files via upload

--- DSA_LAB2_DenominationCalculator/src/com/greatlearning/javafsd/denominations/Main.java
+++ DSA_LAB2_DenominationCalculator/src/com/greatlearning/javafsd/denominations/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("Enter the total number of currency denominations");
+		System.out.println("Enter the size of currency denominations : ");
 		Scanner input = new Scanner(System.in);
 		int size = input.nextInt();
 		int[] denominations = new int[size];
-		System.out.println("Enter the denominations");
+		System.out.println("Enter the currency denominations value : ");
 		for (int i = 0; i < size; i++) {
 			denominations[i] = input.nextInt();
 		}
-		System.out.println("Enter the amount to be paid");
+		System.out.println("Enter the amount you want to pay : ");
 		int amount = input.nextInt();
 
 		DenominationCalculator calculator = new DenominationCalculator(denominations, amount);
