70f0ed683218d43f80523426c1a2129579c456b0
Gowtham233 <65482558+Gowtham233@users.noreply.github.com>
2023-01-11 10:24:00 +0530

    Add files via upload

--- DSA_LAB2_PayMoney/src/com/greatlearning/javafsd/paymoney/Main.java
+++ DSA_LAB2_PayMoney/src/com/greatlearning/javafsd/paymoney/Main.java
@@ -9,24 +9,24 @@ public class Main {
 		try {
 			// Getting user inputs for number of transactions
 			Scanner input = new Scanner(System.in);
-			System.out.println("Enter the number of transactions :");
+			System.out.println("Enter the size of transaction array : ");
 			int transactionArraySize = input.nextInt();
 			int[] transactions = new int[transactionArraySize];
 
 			// Getting user inputs for values of transactions
-			System.out.println("Enter the transaction values:");
+			System.out.println("Enter the values of array :");
 			for (int index = 0; index < transactionArraySize; index++) {
 				transactions[index] = input.nextInt();
 			}
 
 			// Getting user inputs for targets
-			System.out.print("Enter the number of targets to be achieved:");
+			System.out.print("Enter the total no of targets that needs to be achieved : ");
 			int targetInstanceSize = input.nextInt();
 
 			for (int count = 1; count <= targetInstanceSize; count++) {
 
 				System.out.println();
-				System.out.print("Enter the target value :");
+				System.out.print("Enter the target value : ");
 				int dailyTarget = input.nextInt();
 
 				System.out.println("Processing transactions with target value -> " + dailyTarget);
